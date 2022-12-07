65dcd47557ac0eb063aa6eaa4c192299bd9f0a54
wojciech mierzejewski <55141520+mierzvoj@users.noreply.github.com>
2022-12-07 20:51:02 +0100

    test no 11 added

--- Tests.py
+++ Tests.py
@@ -40,9 +40,9 @@ class Test_RomanNumeralsTest(unittest.TestCase):
     def test_90_being_100_10_is_xc(self):
         self.assertEqual(roman(93), "XCIII")
 
-    # def test_100_is_a_single_c(self):
-    #     self.assertEqual(roman(141), "CXLI")
-    #
+    def test_100_is_a_single_c(self):
+        self.assertEqual(roman(141), "CXLI")
+
     # def test_60_being_50_10_is_lx(self):
     #     self.assertEqual(roman(163), "CLXIII")
     #
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 def roman(value):
     roman_map = {
-        1: "I", 4: "IV", 5: "V", 9: "IX", 10: "X", 40: "XL", 50: "L", 90: "XC",}
+        1: "I", 4: "IV", 5: "V", 9: "IX", 10: "X", 40: "XL", 50: "L", 90: "XC", 100: "C",}
     result = ""
     remainder = value
 
