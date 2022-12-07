4e6dcc305ebe8241c56293a084cdb83cadead2a6
wojciech mierzejewski <55141520+mierzvoj@users.noreply.github.com>
2022-12-07 19:39:32 +0100

    1 commit

---
+++ main.py
@@ -0,0 +1,21 @@
+def roman(value):
+    roman_map = {
+        1: "I", }
+    result = ""
+    remainder = value
+
+    for i in sorted(roman_map.keys(), reverse=True):
+        if remainder > 0:
+            multiplier = i
+            roman_digit = roman_map[i]
+
+            times = remainder // multiplier
+            remainder = remainder % multiplier
+            result += roman_digit * times
+    print(result)
+    return result
+
+
+if __name__ == "__main__":
+    inputnumber = 4
+    roman(inputnumber)
