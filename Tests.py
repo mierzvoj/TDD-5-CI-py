daaaaa2849e8356e5ddedeecbd72ccbf8a6bb6bf
wojciech mierzejewski <55141520+mierzvoj@users.noreply.github.com>
2022-12-07 20:47:56 +0100

    test no 8 added

--- Tests.py
+++ Tests.py
@@ -31,9 +31,9 @@ class Test_RomanNumeralsTest(unittest.TestCase):
     def test_48_is_not_50_2_but_rather_40_8(self):
         self.assertEqual(roman(48), "XLVIII")
 
-    # def test_49_is_not_40_5_4_but_rather_50_10_10_1(self):
-    #     self.assertEqual(roman(49), "XLIX")
-    #
+    def test_49_is_not_40_5_4_but_rather_50_10_10_1(self):
+        self.assertEqual(roman(49), "XLIX")
+
     # def test_50_is_a_single_l(self):
     #     self.assertEqual(roman(59), "LIX")
     #
